40f1f495584e31438f04fd0775a5f217a8af13af
jon <jonjoly94@gmail.com>
2020-05-08 11:39:11 -0400

    featuretwo

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let budgetTotal = document.querySelector(".budget-total");
 let budgetForm = document.querySelector(".budget-form");
-let startTotal = 0;
 budgetForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const data = new FormData(budgetForm);
@@ -8,4 +7,37 @@ budgetForm.addEventListener("submit", (event) => {
     console.log(budget);
     budgetTotal.innerHTML = `$${budget}`
     budgetForm.reset();
+})
+
+
+let purchasesForm = document.querySelector(".purchases-container");
+let foodTotal = document.querySelector(".food-total");
+let entertainmentTotal = document.querySelector(".entertainment-total");
+let billsTotal = document.querySelector(".bills-total");
+let clothesTotal = document.querySelector(".clothes-total");
+
+purchasesForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const data = new FormData(purchasesForm);
+    let amount = data.get("amount");
+    let category = data.get("category")
+
+    console.log(amount, category)
+
+
+
+
+    if (category == "food") {
+        foodTotal.innerHTML = `$${amount}`
+    } else if (category == "entertainment") {
+        entertainmentTotal.innerHTML = `$${amount}`
+    }
+    else if (category == "bills") {
+        billsTotal.innerHTML = `$${amount}`
+    }
+    else if (category == "clothes") {
+        clothesTotal.innerHTML = `$${amount}`
+    }
+    purchasesForm.reset();
+    console.log(amount);
 })
